af1f33c59ba8128e8438183bf5ff587da49c938a
SudoSoulS <shadowknightsid@gmail.com>
2026-02-04 11:01:52 +0530

    Feat: increaded rate limit

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 // General rate limiter for all routes
 export const generalLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 120,
+  limit: 2000,
   standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
